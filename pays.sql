0e03a77d799496e9edd04d04a58db4648aa572e7
SireOderable <nicolaslejeune99@gmail.com>
2018-12-15 14:30:18 +0100

    nouveaux fichiers .sql
    représente chaque table de la BD. Cela permet de diviser pour ainsi ne pas se perdre. Pas forcément utile avec le nombre de tuples que l'on a mais comme on nous a demandé de faire comme si on allait rentrer tous les données des JO cela devient utile

---
+++ pays.sql
@@ -0,0 +1,15 @@
+INSERT INTO pays VALUES
+(1, 'Jamaïque'),
+(2, 'Etats-Unis'),
+(3, 'Canada'),
+(4, 'Suisse'),
+(5, 'Danemark'),
+(6, 'France'),
+(7, 'Espagne'),
+(8, 'Inde'),
+(9, 'Japon'),
+(10, 'Italie'),
+(11, 'Hongrie'),
+(12, 'Fédération de Russie'),
+(13, 'Chine'),
+(14, 'Serbie');
---
+++ sport.sql
@@ -0,0 +1,9 @@
+INSERT INTO sport VALUES
+(1, 'Athlétisme - 100m hommes'),
+(2, 'Athlétisme - 100m femmes'),
+(3, 'Aviron - poids légers (4-) hommes'),
+(4, 'Badminton - simple femmes'),
+(5, 'Basketball - hommes'),
+(6, 'Escrime - épée par équipes hommes'),
+(7, 'Gymnastique - Multiple individuel femmes'),
+(8, 'Gymnastique - Multiple par equipes femmes');
