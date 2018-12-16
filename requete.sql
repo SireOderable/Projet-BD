c8a27f686dbcca39829f6564aac192afa1d6b219
Julien BERSAUTER <julien.bersauter@orange.fr>
2018-12-16 16:39:33 +0100

    Update requete.sql
    requete 2

--- requete.sql
+++ requete.sql
@@ -1,4 +1,26 @@
 -- Difficulté * :
 
-select distinct athlete.id_at, nom, prenom from athlete, (select id_equipe from finale where medaille is not null) as A, composition_equipe where athlete.id_pays = (select id_pays from pays where nom_pays = 'Italie') and composition_equipe.id_equipe = A.id_equipe;
+select distinct athlete.id_at, nom, prenom 
+from athlete, (select id_equipe from finale where medaille is not null) as A, composition_equipe 
+where athlete.id_pays = (select id_pays from pays where nom_pays = 'Italie') 
+and composition_equipe.id_equipe = A.id_equipe;
+
+select distinct nom, medaille, S.nom_sport
+from athlete, 
+(SELECT DISTINCT * FROM sport 
+WHERE sport.nom_sport = 'Athletisme - 100m hommes' 
+OR sport.nom_sport = 'Athletisme - 100m femmes' 
+OR sport.nom_sport = 'Athletisme - 200m hommes' 
+OR sport.nom_sport = 'Athletisme - 400m hommes'
+OR sport.nom_sport = 'Athletisme - 200m femmes'
+OR sport.nom_sport = 'Athletisme - 400m femmes') as S, 
+equipe, 
+(select * from finale where medaille is not null) as A,
+composition_equipe
+where equipe.id_equipe = A.id_equipe
+and equipe.id_sport = S.id_sport
+and composition_equipe.id_at = athlete.id_at
+and composition_equipe.id_equipe = equipe.id_equipe;
+
+
 
