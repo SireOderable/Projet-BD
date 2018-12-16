2ecabff061f9df327555ab2e2c83b3839b3e0d35
SireOderable <nicolaslejeune99@gmail.com>
2018-12-16 20:08:14 +0100

    Update requete2.sql

--- requete2.sql
+++ requete2.sql
@@ -10,15 +10,28 @@ select id_pays from athlete,
 
 select distinct id_equipe, id_at, id_pays
 from composition_equipe as c natural join athlete as a
-where a.id_at = c.id_at order by id_equipe;*/
+where a.id_at = c.id_at order by id_equipe;
 
-SELECT avg(resultat)
+SELECT id_pays, avg(resultat)
 
 FROM etape_tournois as t natural join
 (select distinct id_equipe, id_at, id_pays
 from composition_equipe as c natural join athlete as a
 where a.id_at = c.id_at order by id_equipe) as e
 
-WHERE t.id_sport = (select id_sport from sport where nom_sport like '% 200m nage libre%');
+WHERE t.id_sport = (select id_sport from sport where nom_sport like '% 200m nage libre%')
+group by id_pays order by id_pays;*/
+
+SELECT distinct id_pays, medaille
+
+FROM finale natural join
+(select distinct id_equipe, id_at, id_pays
+from composition_equipe as c natural join athlete as a
+where a.id_at = c.id_at order by id_equipe) as e
+
+WHERE medaille is not null order by id_pays;
+
+
+
 
 
