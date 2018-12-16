055035f7b29719107466eb256a88c9e155ed1a54
SireOderable <nicolaslejeune99@gmail.com>
2018-12-16 19:27:49 +0100

    Update requete2.sql

--- requete2.sql
+++ requete2.sql
@@ -1,2 +1,24 @@
-select distinct resultat from etape_tournois, (select id_sport from sport where nom_sport like '%- 200m nage libre%') as S, equipe
-where equipe.id_sport = S.id_sport; and etapes_tournois.id_equipe = equipe.id_equipe;
+/*select avg(resultat) from etape_tournois where id_sport = (select id_sport from sport where nom_sport like '% 200m nage libre%');
+
+select id_at from composition_equipe, 
+(select id_equipe from etape_tournois where id_sport = (select id_sport from sport where nom_sport like '% 200m nage libre%')) as E 
+where composition_equipe.id_equipe = E.id_equipe;
+
+select id_pays from athlete, 
+(select id_at from composition_equipe, (select id_equipe from etape_tournois where id_sport = (select id_sport from sport where nom_sport like '% 200m nage libre%')) as E where composition_equipe.id_equipe = E.id_equipe) as A
+ where athlete.id_at = A.id_at order by id_pays;
+
+select distinct id_equipe, id_at, id_pays
+from composition_equipe as c natural join athlete as a
+where a.id_at = c.id_at order by id_equipe;*/
+
+SELECT avg(resultat)
+
+FROM etape_tournois as t natural join
+(select distinct id_equipe, id_at, id_pays
+from composition_equipe as c natural join athlete as a
+where a.id_at = c.id_at order by id_equipe) as e
+
+WHERE t.id_sport = (select id_sport from sport where nom_sport like '% 200m nage libre%');
+
+
