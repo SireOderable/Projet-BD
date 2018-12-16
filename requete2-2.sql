79df50a1f443a7e3312a2d152612a7138e9e5a04
Julien BERSAUTER <julien.bersauter@orange.fr>
2018-12-16 21:10:53 +0100

    Update requete2-2.sql
    nom pays

--- requete2-2.sql
+++ requete2-2.sql
@@ -1,8 +1,11 @@
-SELECT id_pays, count(medaille)
+SELECT nom_pays, a.c
+FROM 
+(SELECT id_pays, count(medaille) as c
 
 FROM finale natural join
 (select distinct id_equipe, id_pays
 from composition_equipe as c natural join athlete as a
 where a.id_at = c.id_at order by id_equipe) as e
 
-WHERE medaille is not null group by id_pays order by id_pays;
+WHERE medaille is not null group by id_pays order by id_pays) as A, pays
+WHERE a.id_pays = pays.id_pays;
