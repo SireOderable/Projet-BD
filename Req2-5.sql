a9bca5592910152b4a2e41540e185ccabb75669d
Julien BERSAUTER <julien.bersauter@orange.fr>
2018-12-16 18:13:36 +0100

    Create Req2-5.sql
    2.5

---
+++ Req2-5.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT nom_sport, nom FROM sport, equipe, pays, athlete, composition_equipe
+WHERE nb_joueurs = 1
+AND equipe.id_equipe = composition_equipe.id_equipe
+AND composition_equipe.id_at = athlete.id_at
+AND athlete.id_pays = (SELECT id_pays FROM pays WHERE nom_pays ILIKE '%france%')
+AND sport.id_sport = equipe.id_sport
+AND equipe.id_equipe NOT IN (SELECT id_equipe FROM finale WHERE medaille IS NOT NULL);
+
