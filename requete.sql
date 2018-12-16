87cec6ca1b1d08e7bc15548d271abb3003382223
Julien BERSAUTER <julien.bersauter@orange.fr>
2018-12-16 16:44:30 +0100

    Update requete.sql
    correction 3

--- requete.sql
+++ requete.sql
@@ -5,17 +5,18 @@ from athlete, (select id_equipe from finale where medaille is not null) as A, co
 where athlete.id_pays = (select id_pays from pays where nom_pays = 'Italie') 
 and composition_equipe.id_equipe = A.id_equipe;
 
-select distinct nom, medaille, S.nom_sport
+select distinct nom, medaille, S.nom_sport, nom_pays
 from athlete, 
 (SELECT DISTINCT * FROM sport 
 WHERE sport.nom_sport LIKE 'Athletisme%' ) as S, 
 equipe, 
 (select * from finale where medaille is not null) as A,
-composition_equipe
+composition_equipe, pays
 where equipe.id_equipe = A.id_equipe
 and equipe.id_sport = S.id_sport
 and composition_equipe.id_at = athlete.id_at
-and composition_equipe.id_equipe = equipe.id_equipe;
+and composition_equipe.id_equipe = equipe.id_equipe
+and athlete.id_pays = pays.id_pays;
 
 
 
